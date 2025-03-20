3284f8630e64b6e23ee2f56fd844bb794750c548
Chirag Gautam <chirag.gautam-cs21@gla.ac.in>
2025-03-20 22:51:47 +0530

    UC6: Stored daily wage in an array and calculated total wage

--- hello.js
+++ hello.js
@@ -1,39 +1,29 @@
-const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
-const MAX_WORKING_DAYS = 20;
-const MAX_WORKING_HOURS = 160;
+const NUM_OF_WORKING_DAYS = 20;
 
-function getWorkingHours(empCheck) {
+let totalWage = 0;
+let dailyWages = [];
+
+function getWorkingHours() {
+    let empCheck = Math.floor(Math.random() * 3);
     switch (empCheck) {
         case 0:
-            return 0;
+            return 0; // Absent
         case 1:
-            return PART_TIME_HOURS;
+            return PART_TIME_HOURS; // Part time
         case 2:
-            return FULL_TIME_HOURS;
-        default:
-            return 0;
+            return FULL_TIME_HOURS; // Full time
     }
 }
 
-function calculateDailyWage(empCheck) {
-    let empHours = getWorkingHours(empCheck);
-    return empHours * WAGE_PER_HOUR;
+for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+    let dailyHours = getWorkingHours();
+    let dailyWage = dailyHours * WAGE_PER_HOUR;
+    dailyWages.push(dailyWage);
+    totalWage += dailyWage;
 }
 
-let totalEmpWage = 0;
-let totalWorkingDays = 0;
-let totalEmpHours = 0;
-
-while (totalWorkingDays < MAX_WORKING_DAYS && totalEmpHours < MAX_WORKING_HOURS) {
-    let empCheck = Math.floor(Math.random() * 3);
-    let empHours = getWorkingHours(empCheck);
-    totalEmpHours += empHours;
-    totalEmpWage += calculateDailyWage(empCheck);
-    totalWorkingDays++;
-}
-
-console.log(`Total Employee Wage: $${totalEmpWage}`);
-console.log(`Total Working Days: ${totalWorkingDays}`);
-console.log(`Total Working Hours: ${totalEmpHours}`);
+console.log("Daily Wages: ", dailyWages);
+console.log("Total Wage: ", totalWage);
